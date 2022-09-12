b35651857cf72354ecf5a0aafd1f73e461ea017d
Karavaev <kkaravaev@bdo.ca>
2022-09-12 12:12:42 -0400

    changes

---
+++ index.js
@@ -0,0 +1,8 @@
+ReactDOM.render(<h1>Hello root</h1>, document.getElementById("root"));
+console.log(document.getElementById("root"));
+
+// function Component({ text }) {
+//   return <div>{text}</div>;
+// }
+
+// export default Component;
